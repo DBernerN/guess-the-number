bef63ff18778b2c55ad2bf19a1a551892e9708a7
David <david.bnielsen@hotmail.com>
2022-10-11 14:41:33 +0200

    made game work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+//Guess the number
+import "./App.css";
+import { useEffect, useState } from "react";
+import { clear } from "@testing-library/user-event/dist/clear";
 
-function App() {
+/**
+ * 
+ * Feltet bliver tomt og nedenunder får man og videre om gættet ligger under
+ * eller over det rigtige gæt
+ *
+ */
+
+const App = () => {
+  const [randomnumber, setRandomnumber] = useState(
+    Math.floor(Math.random() * 100) + 1
+  );
+  const [guessed, setGuessed] = useState(false);
+  const [gamedone, setgamedone] = useState(false)
+  const [formerguesses, setFormerguesses] = useState([]);
+    const[val, setVal] = useState("")
+    const[error, seterror] = useState("")
+
+
+  const handlekeydown = (e) => {
+    if (e.key === "Enter") {
+      const number = Math.floor(val);
+      if (number === randomnumber) {
+        setGuessed(true);
+        alert("Well done!")
+      } else {
+        if (number<randomnumber) {
+          seterror("The right number is higher")
+        } 
+        if (number>randomnumber) {
+          seterror("The right number is lower")
+        }
+        
+        setFormerguesses((prev) => ([number, ...prev]));
+        setVal("")
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (formerguesses.length === 10) {
+      setgamedone(true)
+      alert("Game Over")
+    }
+
+
+
+  }, [formerguesses])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Velkommen til!</h1>
+      <h1>Gæt et tal fra 1-100</h1>
+      <label>Skriv dit gæt her: </label>
+      <input value={val} onChange={(e) => setVal(e.target.value)}     type="number" name="guessed" onKeyDown={handlekeydown}/>
+      <p>{guessed ? "You guessed it" : "You didnt get it"}</p>
+      <p>{error} </p>
+    
+      {formerguesses.map((n, i) => (
+        <p key={i}>{n}</p>
+      ))}
     </div>
   );
-}
+};
+
+
+
 
 export default App;
