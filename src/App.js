9a3fc476abe878e13b5f1908d45a92300492261e
David <david.bnielsen@hotmail.com>
2022-10-12 14:17:12 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -4,70 +4,88 @@ import { useEffect, useState } from "react";
 import { clear } from "@testing-library/user-event/dist/clear";
 
 /**
- * 
- * Feltet bliver tomt og nedenunder får man og videre om gættet ligger under
- * eller over det rigtige gæt
+ *
+ *
  *
  */
 
 const App = () => {
   const [randomnumber, setRandomnumber] = useState(
-    Math.floor(Math.random() * 100) + 1
+    Math.floor(Math.random() * 1000) + 1
   );
   const [guessed, setGuessed] = useState(false);
-  const [gamedone, setgamedone] = useState(false)
+  const [gamedone, setgamedone] = useState(false);
   const [formerguesses, setFormerguesses] = useState([]);
-    const[val, setVal] = useState("")
-    const[error, seterror] = useState("")
+  const [val, setVal] = useState("");
+  const [error, seterror] = useState("");
 
-
-  const handlekeydown = (e) => {
+  const handlekeydown = async (e) => {
     if (e.key === "Enter") {
       const number = Math.floor(val);
       if (number === randomnumber) {
         setGuessed(true);
-        alert("Well done!")
+        await alert("Godt klaret, du gættede rigtigt!");
+        window.location.reload();
       } else {
-        if (number<randomnumber) {
-          seterror("The right number is higher")
-        } 
-        if (number>randomnumber) {
-          seterror("The right number is lower")
+        if (number < randomnumber) {
+          seterror("Det rigtige tal er højere");
         }
-        
-        setFormerguesses((prev) => ([number, ...prev]));
-        setVal("")
+        if (number > randomnumber) {
+          seterror("Det rigtige tal er lavere");
+        }
+
+        setFormerguesses((prev) => [number, ...prev]);
+        setVal("");
       }
     }
   };
 
   useEffect(() => {
     if (formerguesses.length === 10) {
-      setgamedone(true)
-      alert("Game Over")
+      setgamedone(true);
+      alert("Game Over, det rigtige svar var " + randomnumber);
+      window.location.reload();
     }
-
-
-
-  }, [formerguesses])
+    console.log(randomnumber);
+  }, [formerguesses]);
 
   return (
-    <div className="App">
-      <h1>Velkommen til!</h1>
-      <h1>Gæt et tal fra 1-100</h1>
-      <label>Skriv dit gæt her: </label>
-      <input value={val} onChange={(e) => setVal(e.target.value)}     type="number" name="guessed" onKeyDown={handlekeydown}/>
-      <p>{guessed ? "You guessed it" : "You didnt get it"}</p>
-      <p>{error} </p>
-    
-      {formerguesses.map((n, i) => (
-        <p key={i}>{n}</p>
-      ))}
-    </div>
+    <body>
+      <div className="App">
+        <h1>Velkommen til!</h1>
+        <h1>
+          <em>Gæt et tal fra 1-1000</em>
+        </h1>
+        <label>
+          <strong>Skriv dit gæt her:</strong>
+        </label>
+        <input
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+          type="number"
+          name="guessed"
+          onKeyDown={handlekeydown}
+        />
+
+        <p>
+          {formerguesses.length !== 0
+            ? guessed
+              ? "Rigtigt!"
+              : "Øv! Prøv igen"
+            : ""}
+        </p>
+
+        <p>{error} </p>
+        <img
+          src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
+          width="500"
+        />
+        {formerguesses.map((n, i) => (
+          <p key={i}>{n}</p>
+        ))}
+      </div>
+    </body>
   );
 };
-
-
-
 
 export default App;
